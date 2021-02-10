555075d175856e4b65ef50a5e465c78277922a75
Gregory Olivo <66894900+golivo28@users.noreply.github.com>
2021-02-10 12:41:14 -0500

    Update IX
    Output of PotsFailures.py will be a single .xlsx file.

--- PotsFailures.py
+++ PotsFailures.py
@@ -9,9 +9,9 @@ def failures(df_outliers, df_info, other_2):
                                                              'FailureType']].set_index('Potkey',drop = True)
     
     #¿Qué y cuántas fallas se presentaron?
-    a = df_outliers.groupby('FailureType')[['Duration']].count().rename(columns = {'Duration':'Count'})
-    a.to_excel(r'Documents\Prueba Tekus II\fail1.xlsx')
-    
+    a = df_outliers.join(other = other_2, on = 'Potkey').groupby(['Serial','FailureType',], as_index = False)[['Duration']].\
+    count().rename(columns = {'Duration': 'Count'}).set_index('Serial',drop = True)
+        
     #Proporción de fallas por olla
     df_fail = df_outliers.join(other = other_2, on = 'Potkey').groupby('Serial')[['Duration']].count().\
     rename(columns = {'Duration':'Count'}).sort_values(by = 'Count', ascending = False)
@@ -24,8 +24,8 @@ def failures(df_outliers, df_info, other_2):
     df_percentage['Failure rate (%)'] = np.round(100*df_percentage['Count_fail']/(df_percentage['Count_fail'] +\
                                                                                   df_percentage['Count_good']),2)
 
-    df_percentage = df_percentage.sort_values(by = 'Failure rate (%)', ascending = False)
-
-    df_percentage.to_excel(r'Documents\Prueba Tekus II\fail2.xlsx')
+    b = a.join(other = df_percentage, on = 'Serial')
+    
+    b.to_excel(r'Documents\Prueba Tekus II\fail_t.xlsx')
     
     
